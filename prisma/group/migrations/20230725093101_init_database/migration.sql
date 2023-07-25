2c014996be252ebf7812a7fddc8f29e32afa484d
Khang Nguyễn <thinhkhang97@gmail.com>
2023-07-25 16:43:23 +0700

    feat(group): init group infrastructure and env

--- prisma/group/migrations/20230725093101_init_database/migration.sql
+++ prisma/group/migrations/20230725093101_init_database/migration.sql
@@ -10,18 +10,19 @@ CREATE TABLE "group" (
 );
 
 -- CreateTable
-CREATE TABLE "group_role" (
+CREATE TABLE "role" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "version" INTEGER NOT NULL DEFAULT 0,
     "name" TEXT NOT NULL,
+    "groupId" TEXT NOT NULL,
 
-    CONSTRAINT "group_role_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "role_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "group_member" (
+CREATE TABLE "member" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -31,11 +32,11 @@ CREATE TABLE "group_member" (
     "userId" TEXT NOT NULL,
     "groupId" TEXT NOT NULL,
 
-    CONSTRAINT "group_member_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "member_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "group_member_role" (
+CREATE TABLE "member_role" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -43,32 +44,38 @@ CREATE TABLE "group_member_role" (
     "groupMemberId" TEXT NOT NULL,
     "roleId" TEXT NOT NULL,
 
-    CONSTRAINT "group_member_role_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "member_role_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "group_id_key" ON "group"("id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "group_role_id_key" ON "group_role"("id");
+CREATE UNIQUE INDEX "role_id_key" ON "role"("id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "group_member_id_key" ON "group_member"("id");
+CREATE INDEX "role_groupId_idx" ON "role"("groupId");
 
 -- CreateIndex
-CREATE INDEX "group_member_groupId_idx" ON "group_member"("groupId");
+CREATE UNIQUE INDEX "member_id_key" ON "member"("id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "group_member_role_id_key" ON "group_member_role"("id");
+CREATE INDEX "member_groupId_idx" ON "member"("groupId");
 
 -- CreateIndex
-CREATE INDEX "group_member_role_groupMemberId_roleId_idx" ON "group_member_role"("groupMemberId", "roleId");
+CREATE UNIQUE INDEX "member_role_id_key" ON "member_role"("id");
+
+-- CreateIndex
+CREATE INDEX "member_role_groupMemberId_roleId_idx" ON "member_role"("groupMemberId", "roleId");
 
 -- AddForeignKey
-ALTER TABLE "group_member" ADD CONSTRAINT "group_member_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "role" ADD CONSTRAINT "role_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "group_member_role" ADD CONSTRAINT "group_member_role_groupMemberId_fkey" FOREIGN KEY ("groupMemberId") REFERENCES "group_member"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "member" ADD CONSTRAINT "member_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "group_member_role" ADD CONSTRAINT "group_member_role_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "group_role"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "member_role" ADD CONSTRAINT "member_role_groupMemberId_fkey" FOREIGN KEY ("groupMemberId") REFERENCES "member"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "member_role" ADD CONSTRAINT "member_role_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "role"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
