66c6a371d5814e9beefba97bc4cb250ae5967cc8
Khang Nguyễn <thinhkhang97@gmail.com>
2023-08-14 19:51:19 +0700

    feat(group): :sparkles: implement get members in a group

---
+++ libs/group/application/src/queries/get-members/get-members.query-handler.ts
@@ -0,0 +1,28 @@
+import { Member, MemberRepository } from '@lib/group/domain';
+import { BaseQueryHandler, CUID } from '@lib/shared';
+import { QueryHandler } from '@nestjs/cqrs';
+
+import { GetMembersQuery } from './get-members.query';
+
+@QueryHandler(GetMembersQuery)
+export class GetMembersQueryHandler extends BaseQueryHandler<
+  GetMembersQuery,
+  Member[]
+> {
+  constructor(private readonly _memberRepository: MemberRepository) {
+    super();
+  }
+
+  protected async handle(query: GetMembersQuery): Promise<Member[]> {
+    const userId = new CUID(query.userId);
+    const groupId = new CUID(query.groupId);
+    const memberInGroup = await this._memberRepository.findOne({
+      userId,
+      groupId,
+    });
+    if (!memberInGroup) {
+      return [];
+    }
+    return await this._memberRepository.findMany({ groupId });
+  }
+}
--- libs/group/graphql-ui/src/queries/member.query.ts
+++ libs/group/graphql-ui/src/queries/member.query.ts
@@ -1,10 +1,13 @@
-import { GetMemberQuery } from '@lib/group/application/queries';
+import {
+  GetMemberQuery,
+  GetMembersQuery,
+} from '@lib/group/application/queries';
 import { Member } from '@lib/group/domain';
 import {
   MemberBaseErrorObject,
   MemberObject,
 } from '@lib/group/graphql-ui/objects';
-import { Either } from '@lib/shared';
+import { Either, GraphQLUser, LoggedUser } from '@lib/shared';
 import { QueryBus } from '@nestjs/cqrs';
 import { Args, ID, Query, Resolver } from '@nestjs/graphql';
 
@@ -25,5 +28,26 @@ export class MemberQuery {
       return new MemberBaseErrorObject(result.unwrapErr().message);
     }
     return new MemberObject(result.unwrap());
+  }
+
+  @Query(() => [MemberObject], { name: 'members' })
+  public async getMembers(
+    @Args({ type: () => ID, name: 'groupId' }) groupId: string,
+    @GraphQLUser() loggedUser: LoggedUser,
+  ) {
+    const result = await this._queryBus.execute<
+      GetMembersQuery,
+      Either<Member[]>
+    >(
+      new GetMembersQuery({
+        groupId,
+        userId: loggedUser.id,
+      }),
+    );
+
+    if (result.isErr()) {
+      return [];
+    }
+    return result.unwrap().map((member) => new MemberObject(member));
   }
 }
