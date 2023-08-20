ec06a88eac6eb8b2f433dffd0decd7bd2172681a
Khang Nguyễn <thinhkhang97@gmail.com>
2023-08-20 14:08:49 +0700

    feat(meeting): :sparkles: config redis env

--- apps/socket/src/socket.module.ts
+++ apps/socket/src/socket.module.ts
@@ -1,9 +1,15 @@
 import { PlaningMeetingSocketGateway } from '@lib/meeting/socket';
 import { WrappedCacheModule } from '@lib/shared/modules/wrapped-cache/wrapped-cache.module';
+import { WrappedJwtModule } from '@lib/shared/modules/wrapped-jwt/wrapped-jwt.module';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [WrappedCacheModule.forRedis('./apps/socket/src/.env')],
+  imports: [
+    ConfigModule,
+    WrappedJwtModule.registerAsync(),
+    WrappedCacheModule.forRedis('./apps/socket/src/.env'),
+  ],
   providers: [PlaningMeetingSocketGateway],
 })
 export class SocketModule {}
