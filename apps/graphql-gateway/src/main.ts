6ace780bfe7e37c3efd7f2704a522b153244c293
Khang Nguyễn <thinhkhang97@gmail.com>
2023-08-20 14:08:01 +0700

    feat(app): :sparkles: enable cors

--- apps/graphql-gateway/src/main.ts
+++ apps/graphql-gateway/src/main.ts
@@ -4,6 +4,8 @@ import { GraphqlGatewayModule } from './graphql-gateway.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(GraphqlGatewayModule);
+  app.enableCors({ origin: '*' });
   await app.listen(3000);
 }
+
 bootstrap();
