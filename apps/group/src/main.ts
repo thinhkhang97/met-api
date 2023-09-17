531579ac4f5895be39fbb2c0215d9f485837d9aa
Khang Nguyễn <thinhkhang97@gmail.com>
2023-09-17 19:10:57 +0700

    ci(app): :white_check_mark: test ci env

--- apps/group/src/main.ts
+++ apps/group/src/main.ts
@@ -1,3 +1,5 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
@@ -5,15 +7,24 @@ import { GroupModule } from './group.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(GroupModule);
+  const configService = app.get(ConfigService);
+  const logger = new Logger();
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.TCP,
     options: {
-      // host: '0.0.0.0',
+      host: '0.0.0.0',
       port: 3012,
     },
   });
   await app.startAllMicroservices();
   await app.listen(3002);
+  logger.log(
+    `Started successfully, env: ${configService.get(
+      'ENV',
+    )}, env: ${configService.get(
+      'IDENTITY_INTERNAL_SERVICE',
+    )}, env: ${configService.get('INTERNAL_API_KEY')},`,
+  );
 }
 
 bootstrap();
--- apps/identity/src/main.ts
+++ apps/identity/src/main.ts
@@ -1,9 +1,9 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 import { IdentityModule } from './identity.module';
-import { ConfigService } from '@nestjs/config';
-import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(IdentityModule);
@@ -12,13 +12,19 @@ async function bootstrap() {
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.TCP,
     options: {
-      // host: '0.0.0.0',
+      host: '0.0.0.0',
       port: 3011,
     },
   });
   await app.startAllMicroservices();
   await app.listen(3001);
-  logger.log('Started successfully, env: ' + configService.get('APP_ENV'));
+  logger.log(
+    `Started successfully, env: ${configService.get(
+      'ENV',
+    )}, env: ${configService.get('JWT_SECRET')}, env: ${configService.get(
+      'INTERNAL_API_KEY',
+    )},`,
+  );
 }
 
 bootstrap();
