c74155378ab141674a2f8fd15439a67b2a660a7e
Khang Nguyễn <thinhkhang97@gmail.com>
2023-09-22 10:29:15 +0700

    fix(app): :bug: remove host for deployment

--- apps/identity/src/main.ts
+++ apps/identity/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.TCP,
     options: {
-      host: '0.0.0.0',
+      // host: '0.0.0.0',
       port: 3011,
     },
   });
