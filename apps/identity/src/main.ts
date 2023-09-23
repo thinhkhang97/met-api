c00dbb555171f8653c7b0b1e8f68014624f11fc0
Khang Nguyễn <thinhkhang97@gmail.com>
2023-09-23 16:45:09 +0700

    fix(identity): :bug: revert port

--- apps/identity/src/main.ts
+++ apps/identity/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     transport: Transport.TCP,
     options: {
       // host: '0.0.0.0',
-      port: 3001,
+      port: 3011,
     },
   });
   await app.startAllMicroservices();
