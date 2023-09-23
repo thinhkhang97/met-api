212e72d2a143abfa8614e2c2979ade4389350f65
Khang Nguyễn <thinhkhang97@gmail.com>
2023-09-23 11:33:04 +0700

    build(identity): :sparkles: update identity ms port

--- apps/identity/src/main.ts
+++ apps/identity/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     transport: Transport.TCP,
     options: {
       // host: '0.0.0.0',
-      port: 3011,
+      port: 3001,
     },
   });
   await app.startAllMicroservices();
